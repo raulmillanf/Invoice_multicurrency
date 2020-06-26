d06e47fb12581bb96dfdd1675ddbf7b497e7bbab
Raúl Millán <raulmillanfle@gmail.com>
2020-06-26 19:21:36 -0400

    P1

--- models/models.py
+++ models/models.py
@@ -1,46 +1,26 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
+class InvoiceMulticurrency(models.Model):
+    _inherit = 'account.move.line'
+     
+    price_unit_currency= fields.Float(string='Unit Price', digits='Product Price')
+    price_subtotal_currency = fields.Monetary(string='Subtotal', store=True, readonly=True, currency_field='always_set_currency_id')
+    price_total_currency = fields.Monetary(string='Total', store=True, readonly=True, currency_field='always_set_currency_id')
 
- class InvoiceMulticurrency(models.Model):
-     _inherit = 'account.invoice.line'
-     price_unit_currency = fields.Float(string='Precio Moneda E.', required=True, digits=dp.get_precision('Product Price'))
-     price_subtotal_currency= fields.Monetary(string='Subtotal', store=True, readonly=True, compute='_compute_price', help="Total amount without taxes")
-     price_total_currency = fields.Monetary(string='Total', store=True, readonly=True, compute='_compute_price', help="Total amount with taxes")
+    def _get_computed_price_unit_currency(self):
+        self.ensure_one()
 
-      @api.depends('price_unit', 'discount', 'invoice_line_tax_ids', 'quantity',
-        'product_id', 'invoice_id.partner_id', 'invoice_id.currency_id', 'invoice_id.company_id',
-        'invoice_id.date_invoice', 'invoice_id.date')
-     def _compute_price(self):
-        currency = self.invoice_id and self.invoice_id.currency_id or None
-        price = self.price_unit * (1 - (self.discount or 0.0) / 100.0)
-        taxes = False
-        if self.invoice_line_tax_ids:
-            taxes = self.invoice_line_tax_ids.compute_all(price, currency, self.quantity, product=self.product_id, partner=self.invoice_id.partner_id)
-        self.price_subtotal = price_subtotal_signed = taxes['total_excluded'] if taxes else self.quantity * price
-        self.price_total = taxes['total_included'] if taxes else self.price_subtotal
-        if self.invoice_id.currency_id and self.invoice_id.currency_id != self.invoice_id.company_id.currency_id:
-            price_subtotal_signed = self.invoice_id.currency_id.with_context(date=self.invoice_id._get_currency_rate_date()).compute(price_subtotal_signed, self.invoice_id.company_id.currency_id)
-        sign = self.invoice_id.type in ['in_refund', 'out_refund'] and -1 or 1
-        self.price_subtotal_signed = price_subtotal_signed * sign
-        
-         def _set_taxes(self):
-            """ Used in on_change to set taxes and price."""
-        if self.invoice_id.type in ('out_invoice', 'out_refund'):
-            taxes = self.product_id.taxes_id or self.account_id.tax_ids
+        if not self.product_id:
+            return self.price_unit_currency
+        elif self.move_id.is_sale_document(include_receipts=True):
+            # Out invoice.
+            price_unit = self.product_id.lst_price
+        elif self.move_id.is_purchase_document(include_receipts=True):
+            # In invoice.
+            price_unit = self.product_id.standard_price
         else:
-            taxes = self.product_id.supplier_taxes_id or self.account_id.tax_ids
+            return self.price_unit
 
-        # Keep only taxes of the company
-        company_id = self.company_id or self.env.user.company_id
-        taxes = taxes.filtered(lambda r: r.company_id == company_id)
-
-        self.invoice_line_tax_ids = fp_taxes = self.invoice_id.fiscal_position_id.map_tax(taxes, self.product_id, self.invoice_id.partner_id)
-
-        fix_price = self.env['account.tax']._fix_tax_included_price
-        if self.invoice_id.type in ('in_invoice', 'in_refund'):
-            prec = self.env['decimal.precision'].precision_get('Product Price')
-            if not self.price_unit or float_compare(self.price_unit, self.product_id.standard_price, precision_digits=prec) == 0:
-                self.price_unit = fix_price(self.product_id.standard_price, taxes, fp_taxes)
-                self._set_currency()
+        
